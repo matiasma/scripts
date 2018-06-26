5a103772d6a58e593185d5faf97e8de1786f1806
matiasma <matias_marcelo@live.com>
2018-06-26 15:21:15 -0300

    Update windows-vm-deallocate.ps1

--- windows-vm-deallocate.ps1
+++ windows-vm-deallocate.ps1
@@ -7,6 +7,5 @@ $subscriptionId = $jsonBody.compute.subscriptionId
 $response = Invoke-WebRequest -Uri "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F" -Headers @{Metadata="true"}
 $content =$response.Content | ConvertFrom-Json
 $access_token = $content.access_token
-echo "The MSI access token is $access_token"
 
 Invoke-WebRequest -Uri https://management.azure.com/subscriptions/$($subscriptionId)/resourceGroups/$($resourceGroupName)/providers/Microsoft.Compute/virtualMachines/$($vmname)/deallocate?api-version=2017-12-01 -Method POST -ContentType "application/json" -Headers @{ Authorization ="Bearer $access_token"}
